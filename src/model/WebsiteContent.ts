05b287b59a72a1efb75f5c9ffab3a1acb6e6c9b9
NickSouth <nickrsouthey@gmail.com>
2026-05-04 00:06:46 -0400

    feat: defined types for repo

--- src/model/WebsiteContent.ts
+++ src/model/WebsiteContent.ts
@@ -1,1 +1,29 @@
 export interface IWebsiteContent {}
+export type BigBoard = BigBoardEntry[];
+
+type Position = "QB" | "RB" | "WR" | "TE" | "K" | "OT" | "OG" | "C" | "DE" | "DT" | "LB" | "CB" | "S";
+
+type Height = {
+  feet: number;
+  inches: number;
+};
+
+export type BigBoardEntry = {
+  playerName: string;
+  position: Position;
+  school: string;
+  rank: number;
+  posRank: number;
+  writeup: string;
+  age: number;
+  height: Height;
+  weight: number;
+};
+
+export type Article = {
+  title: string;
+  author: string;
+  publicationDate: Date;
+  content: string;
+  imageUrl?: string;
+};
